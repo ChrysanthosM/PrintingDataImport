48dea07493fa32a88293790a95c98aba037fcd8e
ChrysanthosM <ChrysanthosM@Outlook.com>
2025-11-22 11:14:29 +0200

    Commit

--- src/main/java/org/masouras/ApplicationRun.java
+++ src/main/java/org/masouras/ApplicationRun.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "org.masouras")
 public class ApplicationRun {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRun.class, args);
