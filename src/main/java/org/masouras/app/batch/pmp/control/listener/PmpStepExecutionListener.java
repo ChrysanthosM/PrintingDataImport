565c3bc2e1e74a96ac742190126e7de89966a28e
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-23 20:31:01 +0200

    commit

--- src/main/java/org/masouras/app/batch/pmp/control/listener/PmpStepExecutionListener.java
+++ src/main/java/org/masouras/app/batch/pmp/control/listener/PmpStepExecutionListener.java
@@ -7,6 +7,11 @@ import org.springframework.batch.core.listener.StepExecutionListener;
 import org.springframework.batch.core.step.StepExecution;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 @Slf4j
 @Component
 public class PmpStepExecutionListener implements StepExecutionListener {
@@ -16,10 +21,22 @@ public class PmpStepExecutionListener implements StepExecutionListener {
 
         if (log.isInfoEnabled()) {
             log.info("=== STEP STATISTICS ===");
+            if (stepExecution.getStartTime() != null) log.info("Start Time: {}", stepExecution.getStartTime().toInstant(ZoneOffset.of("+02:00")).atZone(ZoneId.of("Europe/Athens")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSSS")));
+            if (stepExecution.getEndTime() != null) log.info("End Time: {}", stepExecution.getEndTime().toInstant(ZoneOffset.of("+02:00")).atZone(ZoneId.of("Europe/Athens")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSSS")));
+            if (stepExecution.getStartTime() != null && stepExecution.getEndTime() != null) {
+                Duration duration = Duration.between(stepExecution.getStartTime(), stepExecution.getEndTime());
+                log.info("Duration: {}", String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart()));
+            }
+
             log.info("Read count: {}", stepExecution.getReadCount());
             log.info("Write count: {}", stepExecution.getWriteCount());
+            log.info("Commit count: {}", stepExecution.getCommitCount());
+            log.info("Rollback count: {}", stepExecution.getRollbackCount());
             log.info("Skip count: {}", stepExecution.getSkipCount());
-            log.info("Process skip count: {}", stepExecution.getProcessSkipCount());
+            log.info("Read Skip count: {}", stepExecution.getReadSkipCount());
+            log.info("Process Skip count: {}", stepExecution.getProcessSkipCount());
+            log.info("Write skip count: {}", stepExecution.getWriteSkipCount());
+            log.info("Filter count: {}", stepExecution.getFilterCount());
         }
 
         if (stepExecution.getWriteCount() == 0) {
