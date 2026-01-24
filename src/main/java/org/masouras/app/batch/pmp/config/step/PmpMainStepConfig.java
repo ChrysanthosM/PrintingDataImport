c679a87bb23a1d984e934e0149273a2ea9ea8900
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-24 11:25:30 +0200

    commit

--- src/main/java/org/masouras/app/batch/pmp/config/step/PmpMainStepConfig.java
+++ src/main/java/org/masouras/app/batch/pmp/config/step/PmpMainStepConfig.java
@@ -1,9 +1,8 @@
-package org.masouras.app.batch.pmp.config;
+package org.masouras.app.batch.pmp.config.step;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.masouras.app.batch.pmp.control.step.business.processor.PmpMainStepCompositeItemProcessor;
-import org.masouras.app.batch.pmp.control.step.business.processor.PmpReportProcessor;
 import org.masouras.app.batch.pmp.control.step.cycle.PmpMainStepSkipPolicy;
 import org.masouras.app.batch.pmp.control.step.cycle.listener.PmpItemProcessListener;
 import org.masouras.app.batch.pmp.control.step.cycle.listener.PmpProcessListener;
@@ -15,15 +14,14 @@ import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.infrastructure.item.ItemReader;
 import org.springframework.batch.infrastructure.item.ItemWriter;
-import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
-@Configuration
 @RequiredArgsConstructor
-public class PmpStepsConfig {
+@Configuration
+public class PmpMainStepConfig {
     private static final int CHUNK_SIZE = 10;
 
     private final JobRepository jobRepository;
@@ -31,47 +29,28 @@ public class PmpStepsConfig {
     private final ItemReader<PrintingDataEntity> pmpReader;
     private final PmpMainStepCompositeItemProcessor pmpProcessor;
     private final ItemWriter<PrintingDataEntity> pmpWriter;
-    private final PmpReportProcessor pmpReportProcessor;
     private final PmpMainStepSkipPolicy pmpMainStepSkipPolicy;
     private final PmpProcessListener pmpProcessListener;
     private final PmpSkipListener pmpSkipListener;
     private final PmpItemProcessListener pmpItemProcessListener;
     private final PmpStepExecutionListener pmpStepExecutionListener;
 
-
-    @Bean("pmpMainStep")
+    @Bean
     public Step pmpMainStep() {
         return new StepBuilder("pmpMainStep", jobRepository)
                 .<PrintingDataEntity, PrintingDataEntity>chunk(CHUNK_SIZE)
                 .transactionManager(transactionManager)
-
                 .reader(pmpReader)
                 .processor(pmpProcessor)
                 .writer(pmpWriter)
-
                 .faultTolerant()
                 .skipPolicy(pmpMainStepSkipPolicy)
                 .skipLimit(Integer.MAX_VALUE)
                 .skipListener(pmpSkipListener)
                 .allowStartIfComplete(true)
-
                 .listener(pmpProcessListener)
                 .listener(pmpItemProcessListener)
                 .listener(pmpStepExecutionListener)
-                .build();
-    }
-
-
-    // -------------------------------
-    // REPORT STEP (Tasklet)
-    // -------------------------------
-    @Bean("pmpReportStep")
-    public Step pmpReportStep() {
-        return new StepBuilder("pmpReportStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    pmpReportProcessor.process();
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
                 .build();
     }
 }
