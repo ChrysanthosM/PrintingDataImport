761e68c0ead25f410a09a9754e5580deb8641bf3
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-02-07 14:46:07 +0200

    commit

--- src/main/java/org/masouras/PrintingDataImportRun.java
+++ src/main/java/org/masouras/PrintingDataImportRun.java
@@ -3,11 +3,9 @@ package org.masouras;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@EnableScheduling
 public class PrintingDataImportRun {
     public static void main(String[] args) {
         SpringApplication.run(PrintingDataImportRun.class, args);
